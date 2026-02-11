21259effbf566b4b6eafa684e656b6d0e89e7ab3
pkgupta5906-CODING <pkgupta5906@gmail.com>
2026-02-11 20:14:13 +0530

    Variables and keywords

---
+++ Module 1/Lesson 2/Variabales.py
@@ -0,0 +1,11 @@
+n=5
+name= "Manomay Gupta"
+age = 14
+height ="170cm"
+weight ="65kg"
+print("Name is",name )
+print("Age is",age)
+
+#fstring method
+print(f"name is {name}. Age is {age}.")
+print(f"My height is {height}. My weight is {weight}.")
---
+++ Module 1/Lesson 2/printCommands.py
@@ -0,0 +1,4 @@
+print("Let\'s start programming")
+print("one\ntwo\nthree")
+print("4,5,6")
+print("Asterisk  symbol is ",end="*")  #end arguement of print statement.
