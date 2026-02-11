f7b6d84ecb253d0cf387a17911759cf885ddbd2d
pkgupta5906-CODING <pkgupta5906@gmail.com>
2026-02-11 21:01:05 +0530

    birthday storage code

---
+++ Module 1/Lesson 2/Birthday storage.py
@@ -0,0 +1,11 @@
+birthday1 = "January 15, 1990"
+birthday2 = "March 22, 1985"
+birthday3 = "July 4, 2000"
+birthday4 = "November 30, 1995"
+birthday5 = "September 8, 1988"
+
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
