8ee733970f9d632ba9b29577091082eff6da6bde
pkgupta5906-CODING <pkgupta5906@gmail.com>
2026-03-02 23:59:01 +0530

    afterclass

---
+++ Module 2/lesson 10/aftrcealss.py
@@ -0,0 +1,6 @@
+base = int(input("Enter the base number: "))
+power = int(input("Enter the power: "))
+
+result = base ** power
+
+print("Result =", result)
--- Module 2/lesson 10/numberreverse.py
+++ Module 2/lesson 10/numberreverse.py
@@ -1,5 +1,5 @@
 # Write a program to print the numbers in reverse order beginning from the number entered by the user.
 num=int(input("Enter your number : "))
 
-for i in range(num,0,-1):
+for i in range(num,0,-1): # range(start,stop,stepsize)
     print(i)
