c995937aced0490f8c349c4e3f79b0db1ea1700e
pkgupta5906-CODING <pkgupta5906@gmail.com>
2026-03-25 19:47:23 +0530

    dictionary

---
+++ Module 4/Leson 23/sample task.py
@@ -0,0 +1,9 @@
+my_dict={"name":'John Doe',"class":12,"age":15}
+my_dict["age"]=17 # Updating the value of age.
+print(my_dict)
+
+my_dict['gender']='MALE'
+print(my_dict)
+
+my_dict.pop('class')
+print(my_dict)
