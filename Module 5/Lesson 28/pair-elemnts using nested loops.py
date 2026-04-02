a455e221b062197cea773175983c0ad5be49bee2
pkgupta5906-CODING <pkgupta5906@gmail.com>
2026-04-02 17:34:48 +0530

    oops 2.1

---
+++ Module 5/Lesson 28/pair-elemnts using nested loops.py
@@ -0,0 +1,16 @@
+class pair_elemants:
+    def sum(self,nums,sum):
+        lookup={}
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==sum:
+                    return i,j
+
+value=int(input("enter a sum : "))
+obj=pair_elemants()
+result=obj.sum((10,20,30,40,50,60,70),value)
+if result:
+    print("index 1 is equal to",result[0],"index 2 is equal to ",result[1])
+else:
+    print("No pair found !!")
