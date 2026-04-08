ee98789b9b4f30af44c2f37ddaff5d3b7a67e7c7
pkgupta5906-CODING <pkgupta5906@gmail.com>
2026-04-08 15:42:34 +0530

    shape

---
+++ Module 5/Lesson 31/shapes.py
@@ -0,0 +1,17 @@
+#  use polymorphism to implement the shapes classes with same mthod and different output
+class Square:
+    def __init__(self,side):
+        self.side=side
+    def area(self):
+        print("rea of square is",self.side**2)
+class Circle:
+    def __init__(self,radius):
+        self.radius=radius
+    def area (self):
+        print("Area of circle is ",self.radius**2*3.14)
+obj1=Square(12)
+obj2=Circle(12)
+for i in (obj1,obj2):
+    i.area()
+     
+        
