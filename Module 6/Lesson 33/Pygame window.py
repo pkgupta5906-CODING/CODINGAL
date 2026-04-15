ee1707120f3cd871696747914b7b1cfa4fdd2375
pkgupta5906-CODING <pkgupta5906@gmail.com>
2026-04-15 17:11:55 +0530

    changes

--- Module 6/Lesson 33/Pygame window.py
+++ Module 6/Lesson 33/Pygame window.py
@@ -3,6 +3,7 @@
 import pygame
 
 pygame.init()
+pygame.display.set_caption("PyGame Window")
 screen=pygame.display.set_mode((300,200))
 done=False
 while not done:
@@ -10,7 +11,8 @@ while not done:
         if event.type==pygame.QUIT:
             pygame.quit()
             done=True
-    screen.fill((255,255,255))
+    # screen.fill((255,255,255))
+    screen.fill(pygame.Color("red"))
     pygame.display.flip()
 
         
